411cf070b324c27f81d2cb3e82e8acb752fe0d74
Hack2Health <hemashree21.msh@gmail.com>
2024-05-28 14:08:48 +0530

    Added Footer
    Signed-off-by: Hemashree M S <hemashree21.msh@gmail.com>

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,27 +1,42 @@
-import React from 'react'
-import './footer.css';
-
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-function footer() {
+
+const Footer = () => {
   return (
-     <div className='footer'>
-      <div className="fram">
-
-      {/* <div className="github">
-         <a href="https://github.com/AbhiDiva96/75per ">
-          <GitHubIcon/>
-         </a>
-        </div> */}
-
-     <div className="founder">
-      <h4>made by <a href="https://www.linkedin.com/in/abhishek-kumar-062231198/">@AbhiDiva  </a> </h4>
-      </div>
-
-     
-      </div>
+    <div className='bg-white text-black p-10'>
+        <div className='md:flex'>
+            <div className='md:w-1/4'>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
+            75%
+          </span>
+            </div>
+            <div className='md:w-3/4 justify-evenly md:flex'>
+                <div className='md:w-1/3'>
+                    <p className='text-black font-bold mt-8 block w-auto md:mt-0'>Company</p>
+                    <ul className='text-gray-700 py-4'>
+                        <li className='py-1 cursor-pointer'>About Us</li>
+                        <li className='py-1 cursor-pointer'>Contact Us</li>
+                    </ul>
+                </div>
+                <div className='md:w-1/3'>
+                <p className='text-black font-bold w-auto block'>Legal Pages</p>
+                    <ul className='text-gray-700 py-4 cursor-pointer'>
+                        <li className='py-1 cursor-pointer'>Privacy Policy</li>
+                        <li className='py-1 cursor-pointer'>Terms and Conditions</li>
+                        <li className='py-1 cursor-pointer'>Cookie Policy</li>
+                    </ul>
+                </div>
+                <div className='w-1/3'>
+            <div className='md:justify-end'>
+            <a href="https://github.com/AbhiDiva96/75per"><GitHubIcon className='cursor-pointer mr-4'/></a>
+            <a href="https://www.linkedin.com/in/abhishek-kumar-062231198/"><LinkedInIcon className='cursor-pointer'/></a>
+            </div>
+        </div>
+            </div>
+        </div>
+        <div className='mt-10 text-black xl:flex justify-center font-semibold text-sm lg:text-lg xl:text-md'>Copyright @ 75%</div>
     </div>
   )
 }
 
-export default footer
+export default Footer
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
