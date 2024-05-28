b0f2df4dfe49beac0d90582998508edbd806130a
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-05-28 15:18:05 +0530

    enhanched yr Ui

--- src/components/page3/2Year.js
+++ src/components/page3/2Year.js
@@ -2,72 +2,71 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import '../page3/2Year.css'
 import Header from '../../pages/header';
-
-
-import quantumImage from '../../assets/5th.png';
-import youtube from '../../assets/ytt.png'
-import timetable from '../../assets/sec-3A.jpg';
-import syllabus from '../../assets/syllabuswt.jpg';
-
-
+import quantumImage from '../../assets/icons8-book-64.png';
+import timetable from '../../assets/icons8-timetable-64.png';
+import syllabus from '../../assets/icons8-syllabus-80 (1).png';
+import pyq from "../../assets/icons8-minus-1-year-80.png"
+import Notes from "../../assets/icons8-notes-80.png"
+import youtube from "../../assets/icons8-youtube-64.png";
 function sndYear() {
   return (
     <div>
     <Header/>
-
+<div className='background'>
     <div className="yr">
-     <h2>2rd year</h2>
+     <h1>2nd Year</h1>
     </div>
 
-    <div className="content">
+    <div class="content" style={{height:"auto"}}>
       <div class="cards">
-<Link to="/sndquantum">
+<Link class="link" to="/sndquantum">
   <div class="card red">
-  <img src={quantumImage} alt="Quantum" />
+   <img src={quantumImage} alt="" />
       {/* <p class="tip"></p> */}
       <p class="second-text">Quantum</p>
 
   </div>
   </Link>
 
-  <Link to='/sndlecture'>
+  <Link class="link"  to='/sndlecture'>
   <div class="card blue">
-  <img src={youtube} alt="" /> 
+  <img src={youtube} alt="" />
   <p class="second-text">Lecture</p>
   </div>
   </Link>
 
- <Link to='/syllabus'>
+ <Link class="link" to='/syllabus'>
   <div class="card pink">
   <img src={syllabus} alt="" />
     <p class="second-text">Syllabus</p>
   </div> 
   </Link>
 
- <Link to="/pyq">
+ <Link class="link"  to="/pyq">
   <div class="card syan">
    <img src={timetable} alt="" />
     <p class="second-text">Timetable</p>
   </div>
   </Link>
 
- <Link to='/notes'>
+ <Link class="link" to='/notes'>
   <div class="card green">
-      {/* <p class="tip">Hover Me</p> */}
-   <p class="second-text">PYQ</p>
+    <img src={pyq} alt=''/>
+   <p   class="second-text">PYQ</p>
   </div>
   </Link>
 
-<Link to='/notes'>
+<Link  class="link"  to='/notes'>
   <div class="card yellow">
-      <p class="tip"></p>
+  <img src={Notes} alt=''/>
+     
       <p class="second-text">Notes</p>
   </div>
   </Link>
 
 </div>
 </div>
-
+</div>
 {/* <Footer /> */}
   </div>
   )
