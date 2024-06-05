59ae25e4a40ff0108abc04164207a1b06e53544e
Ananya <shavijain2125@gmail.com>
2024-06-06 01:03:05 +0530

    back button added

--- src/components/login/LoginSignup.jsx
+++ src/components/login/LoginSignup.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link
+import { FaArrowLeft } from "react-icons/fa"; // Import Font Awesome arrow left icon
 import "./LoginSignup.css";
 
 const LoginSignup = () => {
-
-
   const [state, setState] = useState("Sign Up");
   const [formData, setFormData] = useState({
     username: "",
@@ -14,44 +14,44 @@ const LoginSignup = () => {
     window.location.href = 'http://localhost:4000/auth/google'; // Redirect to the server route for Google OAuth login
   };
 
-  const ChangeHandler=(e)=>{
-    setFormData({...formData,[e.target.name]:e.target.value})
-  }
+  const ChangeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   const login = async () => {
     console.log("logim");
     let responseData;
-    await fetch("http://localhost:4000/login",{
+    await fetch("http://localhost:4000/login", {
       method: "POST",
-      headers:{
-        Accept:'application/form-data',
-        'content-Type':'application/json'
+      headers: {
+        Accept: 'application/form-data',
+        'content-Type': 'application/json'
       },
       body: JSON.stringify(formData)
-    }).then((response)=>response.json()).then((data)=>responseData=data)
-    if(responseData.success){
-      localStorage.setItem('auth-token',responseData.token);
+    }).then((response) => response.json()).then((data) => responseData = data)
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
       window.location.replace("/")
     }
-    else{
+    else {
       alert(responseData.errors);
     }
   };
   const signup = async () => {
     console.log("Sign up");
     let responseData;
-    await fetch("http://localhost:4000/signup",{
+    await fetch("http://localhost:4000/signup", {
       method: "POST",
-      headers:{
-        Accept:'application/form-data',
-        'content-Type':'application/json'
+      headers: {
+        Accept: 'application/form-data',
+        'content-Type': 'application/json'
       },
       body: JSON.stringify(formData)
-    }).then((response)=>response.json()).then((data)=>responseData=data)
-    if(responseData.success){
-      localStorage.setItem('auth-token',responseData.token);
+    }).then((response) => response.json()).then((data) => responseData = data)
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
       window.location.replace("/")
     }
-    else{
+    else {
       alert(responseData.errors);
     }
   };
@@ -59,12 +59,17 @@ const LoginSignup = () => {
   return (
     <div className="loginsignup">
       <div className="loginsignup-cointainer">
+        {/* Back Icon */}
+        <Link to="/" className="back-icon" style={{ position: 'absolute', top: '20px', left: '40px', fontSize: '32px' }}>
+  <FaArrowLeft />back
+</Link>
+
         <h1>{state}</h1>
         <div className="loginsignup-fields">
           {state === "Sign Up" ? (
             <>
-           <p className="label-login">Name</p>
-            <input type="text" name="username" value={formData.username} onChange={ChangeHandler} placeholder="Your Name" /></>
+              <p className="label-login">Name</p>
+              <input type="text" name="username" value={formData.username} onChange={ChangeHandler} placeholder="Your Name" /></>
           ) : (
             <></>
           )}
@@ -78,7 +83,7 @@ const LoginSignup = () => {
             state === "Login" ? login() : signup();
           }}
         >
-         Sign Up
+          Sign Up
         </button>
         {state === "Sign Up" ? (
           <p className="loginsignup-login">
@@ -99,16 +104,15 @@ const LoginSignup = () => {
                 setState("Sign Up");
               }}
             >
-             Sign Up
+              Sign Up
             </span>
           </p>
         )}
         <div>
-        {state === "Sign Up" ?
-        <button onClick={handleSignInWithGoogle}>Sign Up with Google</button>:
-        <button onClick={handleSignInWithGoogle}>Login with Google</button>}
-      </div>
-
+          {state === "Sign Up" ?
+            <button onClick={handleSignInWithGoogle}>Sign Up with Google</button> :
+            <button onClick={handleSignInWithGoogle}>Login with Google</button>}
+        </div>
       </div>
     </div>
   );
