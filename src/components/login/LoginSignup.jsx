9a757dd2abe1d5f0d6398d61e7180d0314f2f031
Ananya <shavijain2125@gmail.com>
2024-06-09 12:16:01 +0530

    changes done in backk button

--- src/components/login/LoginSignup.jsx
+++ src/components/login/LoginSignup.jsx
@@ -58,11 +58,31 @@ const LoginSignup = () => {
 
   return (
     <div className="loginsignup">
+
+<Link to="/" className="back-icon" style={{ position: 'absolute', top: '15px', left: '35px', fontSize: '42px' }}>
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="42" 
+          height="42" 
+          fill="currentColor" 
+          viewBox="0 0 16 16" 
+          style={{ 
+            fontWeight: 'bold',
+            position: 'fixed', 
+            top: 20, 
+            left: 20
+          }}
+        >
+          <path 
+            fillRule="evenodd" 
+            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+          />
+        </svg>
+      </Link>
+      
       <div className="loginsignup-cointainer">
-        {/* Back Icon */}
-        <Link to="/" className="back-icon" style={{ position: 'absolute', top: '20px', left: '40px', fontSize: '32px' }}>
-  <FaArrowLeft />back
-</Link>
+     
+ 
 
         <h1>{state}</h1>
         <div className="loginsignup-fields">
