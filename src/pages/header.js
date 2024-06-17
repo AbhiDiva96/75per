f768154cc80e6fac26c43cd5abd4f6543ecb8032
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-06-17 23:42:38 +0530

    Created Dark Mode

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './header.css';
 import {Link} from "react-router-dom";
 import { colors } from '@mui/material';
+import { ThemeContext } from '../Content/context';
 // import { useTheme } from '../index';
 
-
+import {HiSun,HiMoon} from 'react-icons/hi2'
 
 function Header() {
-
+    let {theme,toggleTheme} =useContext(ThemeContext)
+    function handleClick(e){
+            if(theme=="dark"){
+                toggleTheme('light')
+            }else{
+                toggleTheme('dark')
+            }
+    }
     //  const {isDarkMode, togglerDarkMode} =useTheme();
 
   return (      
@@ -18,7 +26,7 @@ function Header() {
                 <img src="/logo.png" alt="logo" />
             </Link>
         </div>
-        <div style={{ display: 'flex', gap: '20px' }}>
+        <div style={{ display: 'flex', gap: '20px',flexWrap:"wrap" }}>
             <a href="/Review" className="logo link1" style={{ padding: 10, margin: 5, height: 30, width: 80 }}>
                 <b>Review Us</b>
             </a>
@@ -28,6 +36,10 @@ function Header() {
             <a href="/about" className="logo link1" style={{ padding: 10, margin: 5, height: 30, width: 80 }}>
                 <b>About Us</b>
             </a>
+         {
+
+         theme=='dark'? <HiSun size={"3rem"} onClick={handleClick}/>:<HiMoon size={"3rem"} onClick={handleClick}/>
+         }  
         </div>
     </div>
 </div>
