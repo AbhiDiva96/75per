4f10ce3d70bdfe703fe01bb68f62282cfde2013d
Vipul_Lakum <lakumvipul6351@gmail.com>
2024-06-17 07:59:12 +0530

    updated

--- src/components/page4/trdquantum.js
+++ src/components/page4/trdquantum.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './quantum.css';
-import Header from '../../pages/header.js'
-import Aquantum from '../../assets/5th.png'
+import Header from '../../pages/header.js';
+import ScrollToTopButton from '../ScrollToTopButton.js';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+
+// Import images
+import Aquantum from '../../assets/5th.png';
 import Imgcd from '../../assets/cd.jpg';
 import Imgda from '../../assets/da.png';
 import Imgdbms from '../../assets/dbms.png';
 import Imgdaa from '../../assets/daa1.jpg';
-// import Imgmlt from '../../assets/mlt.jpg'
 import Imgcn from '../../assets/cn.webp';
 import Imgwt from '../../assets/wt.png';
+import Imgitcs from '../../assets/itcsimg.jpg';
 
-import Imgitcs from '../../assets/itcsimg.jpg'
+// Quantum books data
+const quantumBooks = [
+   { img: Imgcd, title: "Compiler Design", file: "cd" },
+   { img: Imgda, title: "Data Analytics", file: "DataAnalytics" },
+   { img: Imgdbms, title: "DBMS", file: "DatabaseManagementSystem" },
+   { img: Imgdaa, title: "DAA", file: "Design&AnalysisOfAlgorithms" },
+   { img: Aquantum, title: "Machine Learning", file: "Machine learning techniques" },
+   { img: Imgcn, title: "Computer Network", file: "ComputerNetwork" },
+   { img: Aquantum, title: "OOPs", file: "oopsquantum" },
+   { img: Imgwt, title: "Web Technology", file: "WEB TECH QUANTUM" },
+   { img: Aquantum, title: "Big Data", file: "BigData" },
+   { img: Aquantum, title: "Software Engineering", file: "sequantum" },
+   { img: Imgitcs, title: "ITCS", file: "itcs" }
+];
 
-import ScrollToTopButton from '../ScrollToTopButton.js';
+function Quantum() {
+   const [searchQuery, setSearchQuery] = useState('');
+   const [filteredBooks, setFilteredBooks] = useState(quantumBooks);
 
+   useEffect(() => {
+      setFilteredBooks(
+         quantumBooks.filter(book =>
+            book.title.toLowerCase().includes(searchQuery.toLowerCase())
+         )
+      );
+   }, [searchQuery]);
 
-
-function quantum() {
-        
-    const handleDownload = (quantum) =>{
-      const syllabusFilePath =  process.env.PUBLIC_URL +  `/quantum/${quantum}.pdf`;
+   const handleDownload = (quantum) => {
+      const syllabusFilePath = process.env.PUBLIC_URL + `/quantum/${quantum}.pdf`;
 
       const link = document.createElement('a');
       link.href = syllabusFilePath;
@@ -27,122 +50,40 @@ function quantum() {
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
- }
+   }
 
-  return (
-
-    <div >
-      <Header />
-        <div style={{ overflow:"auto"}}>
-        <div className="quantum-head">
-          <h2> Download Quantum</h2> 
-        </div>
-      <div className="content" style={{ 
-    height: "auto",overflow:"auto"}}
-  
->
-
-     <div className="book">
-       <img src={Imgcd} alt="" />  
-        <button onClick={() => handleDownload("cd")}>Download</button>
-       <div className="cover">
-        <p>Compiler Design</p>
-     </div>
-  </div>
-
-  <div className="book">
-       <img src={Imgda} alt="" />  
-        <button onClick={() => handleDownload("DataAnalytics")}>Download</button>
-       <div className="cover">
-        <p>Data Analytics</p>
-     </div>
-  </div>
-
-  <div className="book">
-       <img src={Imgdbms} alt="" />  
-        <button onClick={() => handleDownload("DatabaseManagementSystem")}>Download</button>
-       <div className="cover">
-        <p>DBMS</p>
-     </div>
-  </div>
-  
-  <div className="book">
-       <img src={Imgdaa} alt="" />  
-        <button onClick={() => handleDownload("Design&AnalysisOfAlgorithms")}>Download</button>
-       <div className="cover">
-        <p>DAA</p>
-     </div>
-  </div>
-
-  <div className="book">
-     <div className="image">
-       <img src={Aquantum} alt="" />
-       </div>  
-        <button  onClick={()=> handleDownload("Machine learning techniques")}>Download</button>
-       <div className="cover">
-        <p>Machine Learning</p>
-     </div>
-  </div>
-
-  <div className="book">
-       <img src={Imgcn} alt="" /> 
-       
-        <button  onClick={() => handleDownload(
-          "ComputerNetwork"
-        )}>Download</button>
-    
-       <div className="cover">
-        <p>Computer Network</p>
-     </div>
-  </div>
-
-
-  <div className="book">
-       <img src={Aquantum} alt="" />  
-        <button  onClick={()=> handleDownload("oopsquantum")}>Download</button>
-       <div className="cover">
-        <p>OOPs</p>
-     </div>
-  </div>
-
-  <div className="book">
-       <img src={Imgwt} alt="" />  
-        <button  onClick={() => handleDownload("WEB TECH QUANTUM")}>Download</button>
-       <div className="cover">
-        <p>Web Technology</p>
-     </div>
-  </div>
-
-  <div className="book">
-       <img src={Aquantum} alt="" />  
-        <button  onClick={() => handleDownload("BigData")}>Download</button>
-       <div className="cover">
-        <p>Big Data</p>
-     </div>
-  </div>
-
-  <div className="book">
-       <img src={Aquantum} alt="" />  
-        <button onClick={() => handleDownload("sequantum")}>Download</button>
-       <div className="cover">
-        <p>Software Engineering</p>
-     </div>
-  </div>
-
-  
-  <div className="book">
-       <img src={Imgitcs} alt="" />  
-        <button onClick={() => handleDownload("itcs")}>Download</button>
-       <div className="cover">
-        <p>ITCS</p>
-     </div>
-  </div>
-
-</div>
-    </div>
-    <ScrollToTopButton />
-    </div>
-  )
+   return (
+      <div>
+         <Header />
+         <div style={{ overflow: "auto" }}>
+            <div className="quantum-head">
+               <h2>Download Quantum</h2>
+            </div>
+            <div className='inputDiv1'>
+               <FaMagnifyingGlass className='left' />
+               <input
+                  type='text'
+                  className='inputField'
+                  placeholder='Search For Quantum...'
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+               />
+            </div>
+            <div className="content" style={{ height: "auto", overflow: "auto" }}>
+               {filteredBooks.map(book => (
+                  <div className="book" key={book.title}>
+                     <img src={book.img} alt={book.title} />
+                     <button onClick={() => handleDownload(book.file)}>Download</button>
+                     <div className="cover">
+                        <p>{book.title}</p>
+                     </div>
+                  </div>
+               ))}
+            </div>
+         </div>
+         <ScrollToTopButton />
+      </div>
+   )
 }
 
-export default quantum
+export default Quantum;
--- src/components/page6(2nd)/sndlecture.js
+++ src/components/page6(2nd)/sndlecture.js
@@ -1,180 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Header from '../../pages/header';
-
-
+import ScrollToTopButton from '../ScrollToTopButton';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
 
 import Imgmath4 from '../../assets/lmath4.jpg';
 import Imgdsa from '../../assets/ldsa.jpg';
-import Imgcoa from '../../assets/lcoa.jpg'
-import Imgdstl from '../../assets/ldstl.jpg'
-import Imges from '../../assets/les.jpg'
-import Imgtc from '../../assets/ltc.jpg'
-import Imgpy from '../../assets/lpy.jpg'
-import Imgcss from '../../assets/lcss.jpg'
+import Imgcoa from '../../assets/lcoa.jpg';
+import Imgdstl from '../../assets/ldstl.jpg';
+import Imges from '../../assets/les.jpg';
+import Imgtc from '../../assets/ltc.jpg';
+import Imgpy from '../../assets/lpy.jpg';
+import Imgcss from '../../assets/lcss.jpg';
 import Imguhv from '../../assets/luhv.jpg';
 import Imgos from '../../assets/los.jpg';
-
 import Imgtafl from '../../assets/ltafl.jpg';
-
 import ssii from '../../assets/ssii.jpg';
-import ScrollToTopButton from '../ScrollToTopButton';
 
-function sndlecture() {
+const lectures = [
+  { img: Imgmath4, title: "Engineering Mathematics-4", link: "https://www.youtube.com/watch?v=14bNduEh9ag&list=PL5Dqs90qDljXYjZ8kDHtpMqPGKNGb2dxu" },
+  { img: Imgdsa, title: "Data Structure", link: "https://www.youtube.com/watch?v=ouWefOMkMJA&list=PL-JvKqQx2AtcnX8uPrdjvCplAmwTSFhMO" },
+  { img: Imgcoa, title: "Computer Organization & Architecture", link: "https://www.youtube.com/watch?v=8msCz6Nb6nk&list=PL-JvKqQx2Atfuxo1LR0m9RQramPymoBsj" },
+  { img: Imgdstl, title: "Discrete Structure & Theory of Logic", link: "https://www.youtube.com/watch?v=QW2qyX9YU3g&list=PL-JvKqQx2AtcaCRa_z-JeH4FKh1_ZTs_0" },
+  { img: Imges, title: "Energy Science", link: "https://www.youtube.com/watch?v=PcmtbcyDIyg&list=PLRLB5WCqU54W6Gt6NczCeizFGRwLVibFX" },
+  { img: Imgtc, title: "Technical Communication", link: "https://www.youtube.com/watch?v=ZXqi3KqjyMA&list=PLaGBxvvw_eMUhn3H7mlBnqCUcd2Vqgq2s" },
+  { img: Imgpy, title: "Python Programming", link: "https://www.youtube.com/watch?v=Wh3zMk4yAc4&list=PLHEcKKWWhXy_QBXhpQpuqMbtjGVJm_fkt" },
+  { img: Imgcss, title: "Cyber Security", link: "https://www.youtube.com/watch?v=BS5v5Rr-oVo&list=PL-JvKqQx2AteIbm-z4X709scVr9OaHpIY" },
+  { img: Imguhv, title: "Universal Human Values", link: "https://www.youtube.com/watch?v=J1gSYelcM40&list=PLHkk39NrOxRm5ByJViESOeTKWitZS1Hyg" },
+  { img: Imgos, title: "Operating System", link: "https://www.youtube.com/watch?v=i4WyGv5Mbak&list=PL_obO5Qb5QTE9s7QCNjqk97Q4UUgOseWP" },
+  { img: Imgtafl, title: "Theory of Automata & Formal Language", link: "https://www.youtube.com/watch?v=oSCfvEKGUPQ&list=PL_obO5Qb5QTEihQ35PgzjZSh7PveVt-iF" },
+  { img: ssii, title: "Soft Skill-2", link: "https://www.youtube.com/watch?v=TQWspT4kTPc&list=PL8nHpqzOrbFAvPeGY7_PJyoO4xFMs7a8a" },
+];
+
+function SndLecture() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredLectures, setFilteredLectures] = useState(lectures);
+
+  useEffect(() => {
+    setFilteredLectures(
+      lectures.filter(lecture =>
+        lecture.title.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  }, [searchQuery]);
+
   return (
     <div>
-      <Header/>
-     <div className="lecturetrd">
-       <h1> 2nd year lecture</h1>
-
-     </div>
-
-     {/* <div className="lecturelink"> */}
-    
-         
-        <div class="container">
-       
-       {/* compiler Design */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imgmath4} alt="img" />
-                </div>
-              <span class='text-box-in'>Engineering mathematics-4</span>
-             <a href="https://www.youtube.com/watch?v=14bNduEh9ag&list=PL5Dqs90qDljXYjZ8kDHtpMqPGKNGb2dxu">
-                 View Content
+      <Header />
+      <div className="lecturetrd">
+        <h1>2nd year lecture</h1>
+      </div>
+      <div className="inputDiv">
+        <FaMagnifyingGlass className="left" />
+        <input
+          type="text"
+          className="inputField"
+          placeholder="Search For Lectures ..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+      <div style={{ margin: '20px' }}></div>
+      <div className="container">
+        {filteredLectures.map((lecture) => (
+          <div className="box" key={lecture.title}>
+            <div className="upper">
+              <img src={lecture.img} alt={lecture.title} />
+            </div>
+            <span className="text-box-in">{lecture.title}</span>
+            <a href={lecture.link} target="_blank" rel="noopener noreferrer">
+              View Content
             </a>
-        </div>
-
-     {/* data analytics */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imgdsa} alt="img" />
-                </div>
-              <span class='text-box-in'>Data Structure</span>
-             <a href="https://www.youtube.com/watch?v=ouWefOMkMJA&list=PL-JvKqQx2AtcnX8uPrdjvCplAmwTSFhMO">
-                 View Content
-            </a>
-        </div>
-
-
-{/* daa */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imgcoa} alt="img" />
-                </div>
-              <span class='text-box-in'>Computer Organization & Architecture</span>
-             <a href="https://www.youtube.com/watch?v=8msCz6Nb6nk&list=PL-JvKqQx2Atfuxo1LR0m9RQramPymoBsj">
-                 View Content
-            </a>
-        </div>
-
-{/* machine learning */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imgdstl} alt="img" />
-                </div>
-              <span class='text-box-in'>Discrete Structure & theory of logic</span>
-             <a href="https://www.youtube.com/watch?v=QW2qyX9YU3g&list=PL-JvKqQx2AtcaCRa_z-JeH4FKh1_ZTs_0">
-                 View Content
-            </a>
-        </div>
-
-          
-  {/* dbms */}
-  <div class="box">
-         <div className="upper">
-                <img src={Imges} alt="img" />
-                </div>
-              <span class='text-box-in'>Energy Science</span>
-             <a href="https://www.youtube.com/watch?v=PcmtbcyDIyg&list=PLRLB5WCqU54W6Gt6NczCeizFGRwLVibFX">
-                 View Content
-            </a>
-        </div>
-
-{/* computer network */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imgtc} alt="img" />
-                </div>
-              <span class='text-box-in'>Technical Communication</span>
-             <a href="https://www.youtube.com/watch?v=ZXqi3KqjyMA&list=PLaGBxvvw_eMUhn3H7mlBnqCUcd2Vqgq2s">
-                 View Content
-            </a>
-        </div>
-
-{/* oops */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imgpy} alt="img" />
-                </div>
-              <span class='text-box-in'> Python Programming</span>
-             <a href="https://www.youtube.com/watch?v=Wh3zMk4yAc4&list=PLHEcKKWWhXy_QBXhpQpuqMbtjGVJm_fkt">
-                 View Content
-            </a>
-        </div>
-
-{/* web technology */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imgcss} alt="img" />
-                </div>
-              <span class='text-box-in'>Cyber Security</span>
-             <a href="https://www.youtube.com/watch?v=BS5v5Rr-oVo&list=PL-JvKqQx2AteIbm-z4X709scVr9OaHpIY">
-                 View Content
-            </a>
-        </div>
-
-   {/* Big data */}
-        <div class="box">
-         <div className="upper">
-                <img src={Imguhv} alt="img" />
-                </div>
-              <span class='text-box-in'>Universal Human Values</span>
-             <a href="https://www.youtube.com/watch?v=J1gSYelcM40&list=PLHkk39NrOxRm5ByJViESOeTKWitZS1Hyg">
-                 View Content
-            </a>
-        </div>
-
-{/* software engineering */}
-
-<div class="box">
-         <div className="upper">
-                <img src={Imgos} alt="img" />
-                </div>
-              <span class='text-box-in'>Operating System</span>
-             <a href="https://www.youtube.com/watch?v=i4WyGv5Mbak&list=PL_obO5Qb5QTE9s7QCNjqk97Q4UUgOseWP">
-                 View Content
-            </a>
-        </div>
-
-     
-
-        <div class="box">
-         <div className="upper">
-                <img src={Imgtafl} alt="img" />
-                </div>
-              <span class='text-box-in'> Theory of Automata & formal Language</span>
-             <a href="https://www.youtube.com/watch?v=oSCfvEKGUPQ&list=PL_obO5Qb5QTEihQ35PgzjZSh7PveVt-iF">
-                 View Content
-            </a>
-        </div>
-
-        
-        <div class="box">
-         <div className="upper">
-                <img src={ssii} alt="img" />
-                </div>
-              <span class='text-box-in'> soft skill-2</span>
-             <a href="https://www.youtube.com/watch?v=TQWspT4kTPc&list=PL8nHpqzOrbFAvPeGY7_PJyoO4xFMs7a8ahttps://www.youtube.com/watch?v=TQWspT4kTPc&list=PL8nHpqzOrbFAvPeGY7_PJyoO4xFMs7a8ahttps://www.youtube.com/watch?v=TQWspT4kTPc&list=PL8nHpqzOrbFAvPeGY7_PJyoO4xFMs7a8a">
-                 View Content
-            </a>
-        </div>
-
-
-       </div>
-       <ScrollToTopButton />
-     </div>
-    // </div>
-  )
+          </div>
+        ))}
+      </div>
+      <ScrollToTopButton />
+    </div>
+  );
 }
 
-export default sndlecture
+export default SndLecture;
