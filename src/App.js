a27196ece21436e13a1f14673b75842276eec813
codercake <ishithaac2312@gmail.com>
2024-06-18 13:30:06 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React from 'react'
+import {createContext, useState} from 'react';
 import Header from "./pages/header";
+import Timetable from './components/page4/timetable';
 import Footer from "./pages/footer";
 import Home from "./components/page1/Home";
 import Notice from "./components/page2/notice";
-import Timetable from './components/page4/timetable';
 import LoginSignup from './components/login/LoginSignup';
-import CustomChatBot from './components/chatbot/ChatBot';
+import MyChatBot from './components/chatbot/ChatBot';
+
+// import { ThemeProvider } from './components/ThemeContext';
+// const ThemeContext = createContext();
+
 
 function App() {
+
+  // const [theme, setTheme] = useState("dark");
+  // const toggleTheme = () => {
+  //   setTheme((curr) => (curr === "light" ? "light ": "dark"));
+  // }
+ 
+
   return (
-    <div className="maintain">
-      <Header />
-      <Notice />
-      {/* <Timetable /> */}
-      <Home />
-      <CustomChatBot />
-      <Footer />
+<div className="maintain">
+
+    {/* <ThemeContext.Provider value={{theme, toggleTheme}}> */}
+    {/* <div className='app' id={theme}> */}
+       <Header/>
+       <Notice/>
+      
+         {/* <Timetable /> */}
+         <Home />
+         <MyChatBot />
+        <Footer/>
+        
+    {/* </div> */}
+    {/* </ThemeContext.Provider> */}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
