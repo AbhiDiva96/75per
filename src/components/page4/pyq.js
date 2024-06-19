00181783a45f907515d2b1afe494a0fca263eb7c
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2024-06-19 11:54:22 +0530

    added back button on different pages

--- src/components/page4/pyq.js
+++ src/components/page4/pyq.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import Header from "../../pages/header";
 import Footer from "../../pages/footer";
 import './Pyq.css'; 
+import {Link} from 'react-router-dom';
 
 function Pyq() {
   const yearFiles = [
@@ -81,9 +82,30 @@ function Pyq() {
   const { id } = useParams(); 
   const yearItem = yearFiles.find((item) => item.year === id);
   const [selectedSemester, setSelectedSemester] = useState(null);
+  const yearLink={"1st":"/fstyear","2nd":"/sndyear","3rd":"/trdyear","4th":"/fothyear"};
 
   return (
     <div className="page-container">
+      <Link to={yearLink[yearItem.year]} className="back-icon" style={{ position: 'absolute', top: '75px !important', left: '35px', fontSize: '42px' }}>
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="42" 
+          height="42" 
+          fill="currentColor" 
+          viewBox="0 0 16 16" 
+          style={{ 
+            fontWeight: 'bold',
+            position: 'fixed', 
+            top: 130, 
+            left: 20
+          }}
+        >
+          <path 
+            fillRule="evenodd" 
+            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+          />
+        </svg>
+      </Link>
       <Header />
       <div className="container-pyq">
         <h2 className='head'>Download Previous Year Questions</h2>
