9857c6cb55ee3015623c6660d7c6330fb58740d0
kunal maurya <mauryaankit86810@gmail.com>
2024-06-23 11:15:52 +0530

    some changes

--- src/components/page3/4Year.js
+++ src/components/page3/4Year.js
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom';
 import '../page3/2Year.css'
 import Header from '../../pages/header';
 import Footer from '../../pages/footer';
-import quantumImage from '../../assets/icons8-book-64.png';
+import quantumImage from  '../../assets/icons8-book-64.png';
 import timetable from '../../assets/icons8-timetable-64.png';
 import syllabus from '../../assets/icons8-syllabus-80 (1).png';
 import pyq from "../../assets/icons8-minus-1-year-80.png"
 import Notes from "../../assets/icons8-notes-80.png"
 import youtube from "../../assets/icons8-youtube-64.png";
-function sndYear() {
+function FouthYear() {
   return (
     <div>
     <Header/>
@@ -73,4 +73,4 @@ function sndYear() {
   )
 }
 
-export default sndYear
+export default FouthYear;
