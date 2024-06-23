cfc1e94e0d5b1dc15b2317741dd2d987ad39758d
kunal maurya <mauryaankit86810@gmail.com>
2024-06-23 10:51:21 +0530

    4th

--- src/components/page3/4Year.js
+++ src/components/page3/4Year.js
@@ -1,57 +1,76 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import '../page3/2Year.css'
 import Header from '../../pages/header';
-import '../page3/1Year.css';
+import Footer from '../../pages/footer';
+import quantumImage from '../../assets/icons8-book-64.png';
+import timetable from '../../assets/icons8-timetable-64.png';
 import syllabus from '../../assets/icons8-syllabus-80 (1).png';
-
-import quantumImage from '../../assets/5th.png';
-import youtube from '../../assets/ytt.png'
-import timetable from '../../assets/sec-3A.jpg';
-
-
-import ComingSoon from '../../pages/cs.js'
-
-
-function fothYear() {
-
- 
-    return (
-  <div>
-
-  <Header/>
-    
+import pyq from "../../assets/icons8-minus-1-year-80.png"
+import Notes from "../../assets/icons8-notes-80.png"
+import youtube from "../../assets/icons8-youtube-64.png";
+function sndYear() {
+  return (
+    <div>
+    <Header/>
+<div className='background'>
     <div className="yr">
-    <h2>4th year</h2>
+     <h1>4th Year</h1>
     </div>
 
-    <div className="content">
-      <div className="cards">
-  
-  <Link class="link" to='/frthsyllabus'>
-<div class="card pink">
-<img src={syllabus} alt="" />
-  <p class="second-text">Syllabus</p>
-</div> 
-</Link>
-
-  
-
-  
-
-  
-
-  
+    <div class="content" style={{height:"auto"}}>
+      <div class="cards">
+<Link class="link" to="/notes">
+  <div class="card red">
+   <img src={quantumImage} alt="" />
+      {/* <p class="tip"></p> */}
+      <p class="second-text">Quantum</p>
 
   </div>
+  </Link>
+
+  <Link class="link"  to='/notes'>
+  <div class="card blue">
+  <img src={youtube} alt="" />
+  <p class="second-text">Lecture</p>
   </div>
+  </Link>
 
+ <Link class="link" to='/frthsyllabus'>
+  <div class="card pink">
+  <img src={syllabus} alt="" />
+    <p class="second-text">Syllabus</p>
+  </div> 
+  </Link>
+
+ <Link class="link"  to="/notes">
+  <div class="card syan">
+   <img src={timetable} alt="" />
+    <p class="second-text">Timetable</p>
   </div>
+  </Link>
 
-    )
-  }
+ <Link class="link" to='/notes'>
+  <div class="card green">
+    <img src={pyq} alt=''/>
+   <p   class="second-text">PYQ</p>
+  </div>
+  </Link>
 
+<Link  class="link"  to='/notes'>
+  <div class="card yellow">
+  <img src={Notes} alt=''/>
 
+      <p class="second-text">Notes</p>
+  </div>
+  </Link>
 
+</div>
+</div>
+</div>
+<Footer />
+  </div>
+  )
+}
 
-
-export default fothYear;
+export default sndYear
